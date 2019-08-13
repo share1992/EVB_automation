cbae2962092dcaf297f6b6eab9412979ba825e1c
Stephanie Hare <stephanie.hare@bristol.ac.uk>
2019-08-13 12:12:35 +0100

    Completed generation of production trajectory input file

--- squalane_CN_contact.py
+++ squalane_CN_contact.py
@@ -2,11 +2,8 @@ import openbabel
 import numpy as np
 import pandas as pd
 import os
-import sys
 from scipy.spatial.distance import cdist
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.pyplot import cm
+import MDAnalysis as md
 
 # Dictionary of atom INDEXES from VMD. C: (H, (H, (H)))
 all_atoms = {0: (30, 31, 32),
@@ -64,15 +61,15 @@ def read_pdb(path):
     return frames_split
 
 
-def get_atoms_within_threshold_distance(frames_split, threshold_distance):
+def get_atoms_within_threshold_distance(traj_frames_df, threshold_distance):
 
-    C_index = frames_split[0].index[(frames_split[0]['fragment'] == 'CYA') & (frames_split[0]['local index'] == 'C1')][0]
-    N_index = frames_split[0].index[(frames_split[0]['fragment'] == 'CYA') & (frames_split[0]['local index'] == 'N2')][0]
+    C_index = traj_frames_df[0].index[(traj_frames_df[0]['fragment'] == 'CYA') & (traj_frames_df[0]['local index'] == 'C1')][0]
+    N_index = traj_frames_df[0].index[(traj_frames_df[0]['fragment'] == 'CYA') & (traj_frames_df[0]['local index'] == 'N2')][0]
 
     atom_indexes = []
-    for i in range(len(frames_split)):
+    for i in range(len(traj_frames_df)):
         # Distances between CN C and all other atom_indexes for first frame
-        distances = cdist(frames_split[i].iloc[C_index, 6:9][None, :], frames_split[i].iloc[:, 6:9])
+        distances = cdist(traj_frames_df[i].iloc[C_index, 6:9][None, :], traj_frames_df[i].iloc[:, 6:9])
         indexes_within_threshold_distance = np.argwhere(distances < threshold_distance)[:, 1]
 
         for index in indexes_within_threshold_distance:
@@ -83,9 +80,9 @@ def get_atoms_within_threshold_distance(frames_split, threshold_distance):
     return atom_indexes
 
 
-def get_relevant_indices_H_atoms_only(frames_split, atom_indexes):
+def get_relevant_indices_H_atoms_only(traj_frames_df, atom_indexes):
 
-    frame_1 = frames_split[0]
+    frame_1 = traj_frames_df[0]
     local_indexes = []
     fragments = []
 
@@ -106,7 +103,7 @@ def get_relevant_indices_H_atoms_only(frames_split, atom_indexes):
     return relevant_indices_and_fragments_df
 
 
-def generate_patch_defs(H_atoms, atom_dictionary, mol2_file, skeleton_file='squalane-patches_skeleton.txt'):
+def generate_patch_defs(H_atoms, atom_dictionary, squalane_mol2_file, skeleton_file='squalane-patches_skeleton.txt'):
     """
     Print necessary patch definitions for EVB parameters
     :param H_atoms: List of H atoms that get within threshold distance of CN carbon
@@ -122,7 +119,7 @@ def generate_patch_defs(H_atoms, atom_dictionary, mol2_file, skeleton_file='squa
 
     mol = openbabel.OBMol()
 
-    obconversion.ReadFile(mol, mol2_file)
+    obconversion.ReadFile(mol, squalane_mol2_file)
 
     atom_table = {}
     for atom in openbabel.OBMolAtomBFSIter(mol):
@@ -238,49 +235,132 @@ def print_psf_lines(relevant_indices_and_fragments):
             break
 
 
-# TODO: Get this to work (putting necessary lines into input file)
-def print_input_file_lines_to_input_file(relevant_indices_and_fragments, template_file_path):
+def print_patch_lines_to_input_file(relevant_indices_and_fragments, template_file_path):
 
     with open(template_file_path) as template_file, open('test_input.inp', 'w') as output_file:
         output_file.writelines(template_file)
 
-    output_file = open('test_input.inp', 'r+')
+    with open('test_input.inp') as f:
+        content = f.readlines()
 
-    output_file.write("\n")
+    patch_line = content.index("INSERT NECESSARY PATCHES HERE\n")
+
     for i in range(len(relevant_indices_and_fragments)):
         index_1 = relevant_indices_and_fragments['local index'][i]
         index_2 = relevant_indices_and_fragments['fragment index'][i]
 
         line_to_write = "if @NODE .eq. %s PATCH RH%s A %s A 101 setup" % ((i + 1), index_1, index_2)
+        line_number = patch_line + i + 2
 
-        if i == 0:
-            replace_command = 'sed -i s/INSERT NECESSARY PATCHES HERE/%s/g test_input.inp' % line_to_write
-        else:
-            # line_number =
-            replace_command = 'sed -i %si\%s test_input.inp' % (line_number, line_to_write)
+        replace_command = 'sed -i "%si\%s" test_input.inp' % (line_number, line_to_write)
 
-        print(replace_command)
-        # os.system(replace_command)
+        os.system(replace_command)
 
         # Maximum of 16 nodes possible
         if i >= 14:
+            delete_command = 'sed -i %sd test_input.inp' % (patch_line + 1)
+            os.system(delete_command)
             break
 
-    output_file.write("\n")
+
+def print_auto_lines_to_input_file(relevant_indices_and_fragments):
+
+    with open('test_input.inp') as f:
+        content = f.readlines()
+
+    auto_line = content.index("INSERT NECESSARY AUTOGENERATIONS HERE\n")
+
     for i in range(len(relevant_indices_and_fragments)):
-        output_file.write("if @NODE .eq. %s AUTO ANGL DIH" % (i + 1))
+        line_to_write = "if @NODE .eq. %s AUTO ANGL DIH" % (i + 1)
+        line_number = auto_line + i + 2
+
+        replace_command = 'sed -i "%si\%s" test_input.inp' % (line_number, line_to_write)
+
+        os.system(replace_command)
 
         # Maximum of 16 nodes possible
         if i >= 14:
+            delete_command = 'sed -i %sd test_input.inp' % (auto_line + 1)
+            os.system(delete_command)
             break
 
-    output_file.write("\n")
+
+def print_psf_lines_to_input_file(relevant_indices_and_fragments):
+
+    with open('test_input.inp') as f:
+        content = f.readlines()
+
+    psf_line = content.index("INSERT NECESSARY PSF LINES HERE\n")
+
     for i in range(len(relevant_indices_and_fragments)):
-        output_file.write("if @NODE .eq. %s print psf" % (i + 1))
+        line_to_write = "if @NODE .eq. %s print psf" % (i + 1)
+        line_number = psf_line + i + 2
+
+        replace_command = 'sed -i "%si\%s" test_input.inp' % (line_number, line_to_write)
+        os.system(replace_command)
+
+        # Maximum of 16 nodes possible
+        if i >= 14:
+            delete_command = 'sed -i %sd test_input.inp' % (psf_line + 1)
+            os.system(delete_command)
+            break
+
+
+def print_shft_and_coup_lines_to_input_file(relevant_indices_and_fragments, shift_parameter=-25.0,
+                                            coupling_parameter=105.0):
+
+    with open('test_input.inp') as f:
+        content = f.readlines()
+
+    shift_line = content.index("         SHFT 0   0.0 \n")
+
+    for i in range(len(relevant_indices_and_fragments)):
+        line_to_write = "         SHFT %s   %s" % ((i + 1), shift_parameter)
+        line_number = shift_line + i + 2
+
+        replace_command = 'sed -i "%si\%s" test_input.inp' % (line_number, line_to_write)
+        os.system(replace_command)
+
+        if i >= 14:
+            break
+
+    blank_line_command = 'sed -i "%si\ " test_input.inp' % (line_number + 1)
+    os.system(blank_line_command)
+
+    for i in range(len(relevant_indices_and_fragments)):
+        line_to_write = "         COUP 0 %s CONST %s" % ((i + 1), coupling_parameter)
+        new_line_number = line_number + i + 2
+
+        replace_command = 'sed -i "%si\%s" test_input.inp' % (new_line_number, line_to_write)
+        os.system(replace_command)
 
         # Maximum of 16 nodes possible
         if i >= 14:
             break
 
+def generate_probe_trajectory(squalane_simulation_dcd, squalane_simulation_crd, CN_simulation_dcd, CN_simulation_crd):
+
+    return squalane_simulation_crd, CN_simulation_crd
+
+
+def determine_reactive_CH_bonds(probe_trajectory_path, threshold_distance=5.0):
+
+    #TODO: Add line to convert DCD to pdb (or function to use DCD)
+
+    frames = read_pdb(probe_trajectory_path)
+    indexes = get_atoms_within_threshold_distance(frames, threshold_distance)
+    H_atoms = get_relevant_indices_H_atoms_only(frames, indexes)
+
+    return H_atoms
+
+
+def generate_production_trajectory(H_atoms, template_input_path):
+
+    #TODO: Add functions that put probe crd and rst files with production input
+
+    print_patch_lines_to_input_file(H_atoms, template_input_path)
+    print_auto_lines_to_input_file(H_atoms)
+    print_psf_lines_to_input_file(H_atoms)
+    print_shft_and_coup_lines_to_input_file(H_atoms)
 
 
